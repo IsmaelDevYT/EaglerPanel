1b80c24eaa88d0078e2051726d48737689d43709
IsmaelDev <93146246+IsmaelDevYT@users.noreply.github.com>
2024-04-13 09:52:43 -0700

    yup

--- waterfall.sh
+++ waterfall.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 cd waterfall
+wget -O waterfall.jar https://api.papermc.io/v2/projects/waterfall/versions/1.20/builds/562/downloads/waterfall-1.20-562.jar
 while [ true ]; do
     java -Xms4096M -Xmx4096M -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar waterfall.jar --nogui
 
