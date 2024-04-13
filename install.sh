a1b3fc88def7275a5cc43d9cfcf59dc6418226d5
IsmaelDev <93146246+IsmaelDevYT@users.noreply.github.com>
2024-04-13 09:52:27 -0700

    frrrr

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
-sudo -i
 sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt auto-clean -y
 sudo apt-get install default-jdk
 cd server
 sdk install java 17.0.9-amzn
 wget -O server.jar https://api.purpurmc.org/v2/purpur/1.17.1/1428/download
-sudo -i
-cd waterfall
-wget -O waterfall.jar https://api.papermc.io/v2/projects/waterfall/versions/1.20/builds/562/downloads/waterfall-1.20-562.jar
