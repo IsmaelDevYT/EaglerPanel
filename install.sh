0b142942bdfa8e5d7b7e64f590d6bd93c1fe8111
IsmaelDev <93146246+IsmaelDevYT@users.noreply.github.com>
2024-04-13 10:26:14 -0700

    fr

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt update -y && sudo apt upgrade -y && sudo apt autoremove -y && sudo apt auto-clean -y
-sudo apt-get install default-jdk
+sudo apt-get install default-jdk -y
 cd server
 sdk install java 17.0.9-amzn
 wget -O server.jar https://api.purpurmc.org/v2/purpur/1.17.1/1428/download
