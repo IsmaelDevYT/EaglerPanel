e8111ca6f7d6fcd9c0255708ce4bb32c0099920a
IsmaelDev <93146246+IsmaelDevYT@users.noreply.github.com>
2024-04-12 18:36:56 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-$ mkdir -p /var/lib/pufferpanel
-$ docker volume create pufferpanel-config
-$ docker create --name pufferpanel -p 8080:8080 -p 5657:5657 -v pufferpanel-config:/etc/pufferpanel -v /var/lib/pufferpanel:/var/lib/pufferpanel -v /var/run/docker.sock:/var/run/docker.sock --restart=on-failure pufferpanel/pufferpanel:latest
-$ docker start pufferpanel
-$ docker exec -it pufferpanel /pufferpanel/pufferpanel user add
+mkdir -p /var/lib/pufferpanel
+docker volume create pufferpanel-config
+docker create --name pufferpanel -p 8080:8080 -p 5657:5657 -v pufferpanel-config:/etc/pufferpanel -v /var/lib/pufferpanel:/var/lib/pufferpanel -v /var/run/docker.sock:/var/run/docker.sock --restart=on-failure pufferpanel/pufferpanel:latest
+docker start pufferpanel
+docker exec -it pufferpanel /pufferpanel/pufferpanel user add
