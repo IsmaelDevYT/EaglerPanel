c4053cadc1c772abcf07b91fb9fcc8a073d8a828
IsmaelDev <93146246+IsmaelDevYT@users.noreply.github.com>
2024-04-13 09:29:51 -0700

    fr

--- waterfall.sh
+++ waterfall.sh
@@ -1,1 +1,8 @@
+#!/bin/bash
 
+while [ true ]; do
+    java -Xms4096M -Xmx4096M -XX:+UseG1GC -XX:G1HeapRegionSize=4M -XX:+UnlockExperimentalVMOptions -XX:+ParallelRefProcEnabled -XX:+AlwaysPreTouch -XX:MaxInlineLevel=15 -jar server.jar --nogui
+
+    echo Server restarting...
+    echo Press CTRL + C to stop.
+done
